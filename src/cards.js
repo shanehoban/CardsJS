767684e652efc0b9543ec3139fb241ddfa0e36a9
Hoban, Shane <Shane.Hoban@fmr.com>
2016-08-26 15:07:31 +0100

    initial commit

---
+++ src/cards.js
@@ -0,0 +1,17 @@
+var CardsJS = function(){
+    var newDeck = function(numberOfDecks = 1){
+        var suits = ['hearts','diamonds','clubs','spades',];
+        var special = ['jack','queen','king','ace',];
+        var decks = [];
+
+        for(var i = 0; i < numberOfDecks; i++){
+
+        }
+
+        return decks;
+    };
+
+    return{
+        newDeck: newDeck,
+    }
+}();
